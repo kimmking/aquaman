fbae88d8f7e06fdf80e214567d24388f3bcc6ee0
kimmking <kimmking@163.com>
2018-08-27 16:02:19 +0800

    add log&threadpool&gradle fatjar

--- src/main/java/io/github/kimmking/cloud/gateway/vertx/GatewayApplication.java
+++ src/main/java/io/github/kimmking/cloud/gateway/vertx/GatewayApplication.java
@@ -2,18 +2,24 @@ package io.github.kimmking.cloud.gateway.vertx;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
-import io.vertx.core.VertxOptions;
+
 
 public class GatewayApplication extends AbstractVerticle {
     public static void main(String[] args) {
-//        Vertx.vertx().createHttpServer().requestHandler(req -> req.response().
-//                end("Hello World!")).listen(8888);
 
-        int port = 8000;
+        System.setProperty("vertx.logger-delegate-factory-class-name",
+                "io.vertx.core.logging.SLF4JLogDelegateFactory");
+
+
+        String proxyServer = System.getProperty("proxyServer","http://localhost:8088");
+        String proxyPort = System.getProperty("proxyPort","8000");
+
+        int port = Integer.parseInt(proxyPort);
 
         System.out.println("Vert.X starting...");
-        Vertx.vertx(new VertxOptions().setWorkerPoolSize(40)).createHttpServer().
-                requestHandler(new ProxyHandler("http://localhost:8088/")).listen(port);
-        System.out.println("Vert.X started at port:" + port);
+        Vertx.vertx().createHttpServer().
+                requestHandler(new ProxyHandler(proxyServer)).listen(port);
+        System.out.println("Vert.X started at port:" + port + " for server:" + proxyServer);
+
     }
 }
--- src/main/java/io/github/kimmking/cloud/gateway/vertx/ProxyHandler.java
+++ src/main/java/io/github/kimmking/cloud/gateway/vertx/ProxyHandler.java
@@ -6,56 +6,39 @@ import io.vertx.core.http.HttpServerRequest;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
-import org.apache.commons.lang.StringUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.concurrent.FutureCallback;
-import org.apache.http.conn.ConnectionKeepAliveStrategy;
-import org.apache.http.entity.ContentType;
-import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.DefaultRedirectStrategy;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.http.impl.nio.client.HttpAsyncClients;
 import org.apache.http.impl.nio.reactor.IOReactorConfig;
 import org.apache.http.protocol.HTTP;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.Args;
-import org.apache.http.util.ByteArrayBuffer;
-import org.apache.http.util.EntityUtils;
 
 import java.io.InputStream;
-import java.nio.charset.Charset;
 import java.util.concurrent.*;
 
 public class ProxyHandler implements Handler<HttpServerRequest> {
 
     private static Logger logger = LoggerFactory.getLogger(ProxyHandler.class);
-    private static final int HTTP_OK = 200;
-
-    public static final long MAX_CLIENT_LIVE_TIME_MS = 1000 * 60 * 5;
-    private static final long MAX_LAST_FETCH_INTERVAL_MS = 1000 * 15;
-    private static final long FETCH_INTERVAL_SECOND = 6;
-    private static final Charset DEFAULT_CHARSET = Charset.forName("UTF-8");
 
     private CloseableHttpAsyncClient httpclient;
-    private ExecutorService fetchService;
-    private ExecutorService fetchWorker;
-    private String backendUrl  = "";
+    private ExecutorService proxyService;
+    private String backendUrl;
 
     public ProxyHandler(String backendUrl){
         this.backendUrl = backendUrl.endsWith("/")?backendUrl.substring(0,backendUrl.length()-1):backendUrl;
         int cores = Runtime.getRuntime().availableProcessors() * 2;
         long keepAliveTime = 1000;
         int queueSize = 2048;
-        RejectedExecutionHandler handler = new ThreadPoolExecutor.DiscardPolicy();
-        fetchService = new ThreadPoolExecutor(cores, cores,
+        RejectedExecutionHandler handler = new ThreadPoolExecutor.CallerRunsPolicy();//.DiscardPolicy();
+        proxyService = new ThreadPoolExecutor(cores, cores,
                 keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
-                new NamedThreadFactory("proxy-fetchService"), handler);
-        fetchWorker = new ThreadPoolExecutor(cores, cores,
-                keepAliveTime, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(queueSize),
-                new NamedThreadFactory("proxy-fetchWorker"), handler);
+                new NamedThreadFactory("proxyService"), handler);
+
         IOReactorConfig ioConfig = IOReactorConfig.custom()
                 .setConnectTimeout(1000)
                 .setSoTimeout(1000)
@@ -79,9 +62,9 @@ public class ProxyHandler implements Handler<HttpServerRequest> {
 
 
     @Override
-    public void handle(HttpServerRequest request) {
-        String url = this.backendUrl + request.path();
-        fetchGet(request,url);
+    public void handle(final HttpServerRequest request) {
+        final String url = this.backendUrl + request.path();
+        proxyService.submit(()->fetchGet(request,url));
     }
 
     private void fetchGet(final HttpServerRequest inbound,final String url) {
